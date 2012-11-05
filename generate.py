3bb81f419fa3eac270e2e1a8088674fd554acb4e
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2012-11-05 10:53:43 +0100

    Gracefully fail when no data set is supplied

--- generate.py
+++ generate.py
@@ -26,22 +26,28 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate phantoms and \
             sinograms from ellipses model.')
 
-    parser.add_argument('-d', '--predefined', type=str, metavar='DATASET')
+    parser.add_argument('-d', '--predefined', type=str, metavar='DATASET', required=True)
     parser.add_argument('-i', '--input-file', type=str, metavar='FILE')
-    parser.add_argument('-s', '--sinogram', action='store_true')
-    parser.add_argument('-p', '--phantom', action='store_true')
     parser.add_argument('--width', type=int, required=True)
     parser.add_argument('--height', type=int, required=True)
     parser.add_argument('-a', '--angle-step', type=float)
 
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-s', '--sinogram', action='store_true')
+    group.add_argument('-p', '--phantom', action='store_true')
+
     args = parser.parse_args()
-    ds = phantoms.predefined[args.predefined]
+
+    try:
+        ds = phantoms.predefined[args.predefined]
+    except:
+        print "Data set `%s' is not defined." % args.predefined
+        sys.exit(1)
 
     if args.sinogram:
         sinogram = phantoms.sinogram(args.width, np.pi / args.height, args.height, ds)
         tif = TIFF.open('sinogram-%ix%i.tif' % (args.width, args.height), mode='w')
         tif.write_image(sinogram.astype(np.float32))
-
     if args.phantom:
         phantom = phantoms.slice(args.width, args.height, ds)
         tif = TIFF.open('phantom-%ix%i.tif' % (args.width, args.height), mode='w')
